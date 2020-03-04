fd5dcdd666919b7cc102261ec577ee4475b409a8
rd144 <rossdingwall2014@gmail.com>
2020-03-03 17:15:57 -0800

    Added first pass at commenting. Still needs further polishing before pulling onto master

--- URLJobWatch/JobWatch.py
+++ URLJobWatch/JobWatch.py
@@ -1,7 +1,37 @@
+"""
+Created by Ross Dingwall. JobWatch.py is a WebScraper that takes in a config and searches the url's contained in it for
+user determined terms.
+
+Long Term Goals:
+
+    1 - Make and maintain a SQL database from the code itself
+    2 - Check if a Job posting's status has been updated
+    3 - Email a provided user
+
+To do this, place the functionality into a class that can be called from other scripts.
+
+#TODO - Parameterise and Functionalise
+#TODO - Test on multiple URL's
+#TODO - Stress Test on incorrect parameters
+#TODO - Make a SQL Database and maintain it from within the code.
+#TODO - Send Email if Job Updated.
+"""
+
 import requests
 from bs4 import BeautifulSoup
 
 def url_checker(url,watch_list,findall_name,findall_attribs=None):
+    """
+    Checks the URL provided using the parameters provided.
+
+    TODO - Split up into sub-functions
+
+    :param url: 
+    :param watch_list:
+    :param findall_name:
+    :param findall_attribs:
+    :return:
+    """
 
     response = requests.get(url)
 
@@ -16,6 +46,7 @@ def url_checker(url,watch_list,findall_name,findall_attribs=None):
             results_found.append(result.text)
 
     print("Found {count} results".format(count=len(results_found)))
+
     if len(results_found) != 0:
         return True
     else:
