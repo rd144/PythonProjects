214c7fc30a38aacb7a25d8169ac521af625f88c9
rd144 <rossdingwall2014@gmail.com>
2020-03-27 14:58:58 -0700

    Added feedback to the console, and added functionality to maintain a log of the past 100 posts, to ensure duplicate posts are not sent out in an overlap of the 24h period. This will breakdown if the code is run multiple times and accrues more than 100 posts,a t which point it will start to see the most popular ones again.

--- RedditWritingPrompts/Reader.py
+++ RedditWritingPrompts/Reader.py
@@ -1,93 +1,164 @@
 class RedditEmailer():
 
-    def __init__(self,reddit_config,email_config):
+    def __init__(self,reddit_config,email_config,previous_posts_path):
+        import os
+
+        print('RedditEmailer Class Initiating')
 
         self.reddit_config = reddit_config
         self.email_config = email_config
+        self.previous_posts_path = previous_posts_path
+        print('Arguments saved to self')
+
+        if os.path.isfile(previous_posts_path):
+            print('Previous posts file found')
+            file = open(previous_posts_path,'r',encoding='utf-8')
+            self.previous_posts = [line.strip() for line in file]
+            file.close()
+            print('Previous posts loaded')
+        else:
+            print('Previous posts file found')
+            file = open(previous_posts_path, 'w')
+            self.previous_posts = []
+            file.close()
+            print('Previous posts file created at {path}'.format(path=previous_posts_path))
+
+        print('Initiation Complete')
 
     def create_reddit_object(self):
         import praw
+        print("Creating Reddit Object")
+        try:
+            reddit = praw.Reddit(
+                client_id=self.reddit_config["client_id"],
+                client_secret=self.reddit_config["client_secret"],
+                user_agent=self.reddit_config["user_agent"],
+                username=self.reddit_config["username"],
+                password=self.reddit_config["password"]
+            )
+            print("Creation Complete")
+            return reddit
+        except Exception as error:
+            print("Reddit Object Creation failed due to error:\n\t{error}".format(error=error))
+            quit()
 
-        reddit = praw.Reddit(
-            client_id=self.reddit_config["client_id"],
-            client_secret=self.reddit_config["client_secret"],
-            user_agent=self.reddit_config["user_agent"],
-            username=self.reddit_config["username"],
-            password=self.reddit_config["password"]
-        )
+    def subreddit_list_scraper(self,reddit,subreddit,start_text=None,count=10,previous_posts=[]):
 
-        return reddit
+        print("Scraping posts from {sub}".format(sub=subreddit))
+        try:
+            subreddit = reddit.subreddit(subreddit)
+            submission_list = []
+            print("Subreddit: {sub} Found".format(sub=subreddit))
 
-    def subreddit_list_scraper(self,reddit,subreddit,start_text=None,count=10):
+            for index,submission in enumerate(subreddit.top(limit=1000)):
+                text = submission.title
 
-        subreddit = reddit.subreddit(subreddit)
-        submission_list = []
+                if text not in previous_posts:
+                    if start_text:
+                        if text.startswith(start_text):
+                            previous_posts.append(text)
+                            submission_list.append(submission)
+                    else:
+                        previous_posts.append(text)
+                        submission_list.append(submission)
 
-        for index,submission in enumerate(subreddit.top(limit=1000)):
-            text = submission.title
-            if start_text:
-                if text.startswith(start_text):
-                    submission_list.append(submission)
-            else:
-                submission_list.append(submission)
+                if len(submission_list) >= count:
+                    print("Found {count} posts".format(count=count))
+                    break
 
-            if len(submission_list) >= count:
-                break
+            print("Subreddit scraping complete".format(count=count))
+            return submission_list,previous_posts
+        except Exception as error:
+            print("Scraping failed due to error:\n\t{error}".format(error=error))
+            quit()
 
-        return submission_list
+    def previous_post_update(self,previous_posts,previous_posts_path):
+
+        print("Updating the PreviousPosts file")
+        try:
+            # De-dupe the previous posts list
+            previous_posts = list(set(previous_posts))
+            # Check if there are over 100 previous posts, and if so take the most recent 100
+            if len(previous_posts) > 100:
+                previous_posts = previous_posts[-100:]
+
+            print("Writing previous posts to file")
+            file = open(previous_posts_path, 'w', encoding='utf-8')
+            file.write("\n".join(previous_posts))
+            file.close()
+            print("Update Complete")
+        except Exception as error:
+            print("Update Incomplete due to error:\n\t{error}".format(error=error))
+            quit()
 
     def message_creation(self,submission_list,subject,removal_expressions=None):
         import re
         from email.mime.multipart import MIMEMultipart
         from email.mime.text import MIMEText
 
-        email_message = MIMEMultipart("alternative")
-        email_message["Subject"] = subject
+        print("Creating message from Submissions")
 
-        if removal_expressions:
-            pattern_list = []
-            for expression in removal_expressions:
-                pattern_list.append(re.compile(expression))
-        line_list = []
-        for index, submission in enumerate(submission_list):
-            message = submission.title
-            if pattern_list:
-                for expression in pattern_list:
-                    message = expression.sub("", message)
+        try:
+            email_message = MIMEMultipart("alternative")
+            email_message["Subject"] = subject
+            pattern_list = None
 
-            line = "{number}. {url}\nPost:{message}\n\n".format(
-                number=index+1,
-                url=submission.url,
-                message=message
+            if removal_expressions:
+                pattern_list = []
+                for expression in removal_expressions:
+                    pattern_list.append(re.compile(expression))
+            line_list = []
+            for index, submission in enumerate(submission_list):
+                message = submission.title
+                if pattern_list:
+                    for expression in pattern_list:
+                        message = expression.sub("", message)
+
+                line = "{number}. {url}\nPost:{message}\n\n".format(
+                    number=index+1,
+                    url=submission.url,
+                    message=message
+                )
+                line_list.append(line)
+
+            email_message.attach(
+                MIMEText("".join(line_list),'plain','utf-8')
             )
-            line_list.append(line)
 
-        email_message.attach(
-            MIMEText("".join(line_list),'plain','utf-8')
-        )
+            print("Message successfully created from {number} submissions".format(number=len(submission_list)))
 
-        return email_message
+            return email_message
+        except Exception as error:
+            print("Message creation failed due to error:\n\t{error}".format(error=error))
+            quit()
 
-    def send_emails(self,message):
+    def send_emails(self,message,fromEmail,fromPassword,distributionList):
         import smtplib
 
-        session = smtplib.SMTP('smtp.gmail.com', 587)
-        session.ehlo()
-        session.starttls()
-        session.ehlo()
-        session.login(
-            user=self.email_config["fromEmail"],
-            password=self.email_config["fromPassword"]
-        )
-
-        distribution_list = self.email_config["distributionList"]
-
-        for toEmail in distribution_list:
-            session.sendmail(
-                msg=message.as_string(),
-                to_addrs=toEmail,
-                from_addr=self.email_config["fromEmail"]
+        print("Sending message to distribution list")
+        try:
+            print("Creating Gmail Session")
+            session = smtplib.SMTP('smtp.gmail.com', 587)
+            session.ehlo()
+            session.starttls()
+            session.ehlo()
+            session.login(
+                user=fromEmail,
+                password=fromPassword
             )
+            print("Session Created and Logged in")
+
+            print("Sending emails")
+            for toEmail in distributionList:
+                session.sendmail(
+                    msg=message.as_string(),
+                    to_addrs=toEmail,
+                    from_addr=fromEmail
+                )
+                print("Email sent to {toEmail}".format(toEmail=toEmail))
+        except Exception as error:
+            print("Email distribution failed due to error:\n\t{error}".format(error=error))
+            quit()
 
 def arguments():
     import argparse
@@ -96,13 +167,14 @@ def arguments():
 
     parser.add_argument("--subreddit")
     parser.add_argument("--config_path")
+    parser.add_argument("--previous_posts_path",default=None)
     parser.add_argument("--subject",default="PythonGeneratedEmail")
     parser.add_argument("--start_text",default=None)
     parser.add_argument("--count", default=10,type=int)
 
     return parser.parse_args()
 
-def main(config_path,subreddit,start_text,count,subject):
+def main(config_path,subreddit,start_text,count,subject,previous_posts_path):
     import json
     import datetime
 
@@ -111,25 +183,39 @@ def main(config_path,subreddit,start_text,count,subject):
     file.close()
 
     session = RedditEmailer(
-        config["connection_details"],
-        config["email_config"]
+        reddit_config = config["connection_details"],
+        email_config = config["email_config"],
+        previous_posts_path = previous_posts_path
     )
     reddit = session.create_reddit_object()
-    submission_list = session.subreddit_list_scraper(
+    submission_list,previous_posts = session.subreddit_list_scraper(
         reddit=reddit,
         subreddit=subreddit,
         start_text = start_text,
-        count=count
+        count=count,
+        previous_posts=session.previous_posts
     )
+
+    session.previous_post_update(
+        previous_posts=previous_posts,
+        previous_posts_path=session.previous_posts_path
+    )
+
     message = session.message_creation(
         submission_list = submission_list,
         subject="{subject} - {date}".format(
             subject=subject,
-            date=datetime.datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S')
+            date=datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         ),
         removal_expressions=config["email_config"]["removalExpressions"]
     )
-    session.send_emails(message)
+
+    session.send_emails(
+        message=message,
+        fromEmail=session.email_config["fromEmail"],
+        fromPassword=session.email_config["fromPassword"],
+        distributionList=session.email_config["distributionList"]
+    )
 
 if __name__ == '__main__':
     args = arguments()
@@ -138,5 +224,6 @@ if __name__ == '__main__':
         config_path=args.config_path, # Path to the config
         subject=args.subject, # Subject you want for the Email
         start_text=args.start_text, # Start text for the posts to filter by (i.e [WP] is the start of titles on r/WritingPrompts
-        count=args.count # Number of responses you want to send
+        count=args.count,# Number of responses you want to send
+        previous_posts_path = args.previous_posts_path # Path to the text file containing a list of previous posts
     )
