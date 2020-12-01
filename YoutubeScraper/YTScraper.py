8eb089b670d9179dfc37c385b5a2fb196ecb517c
rd144 <rossdingwall2014@gmail.com>
2020-12-01 11:46:47 -0800

    Condensed YTScraper into a usable function that can be called from the command line

--- YoutubeScraper/YTScraper.py
+++ YoutubeScraper/YTScraper.py
@@ -1,22 +1,36 @@
 import pytube
 import re
-from datetime import datetime
+import argparse
 
-playlist_url = 'https://www.youtube.com/playlist?list=PLui6Eyny-UzwxbWCWDbTzEwsZnnROBTIL'
-dir_name = "30DaysOfYoga"
-start = datetime.utcnow()
+def playlist_scraper(url,output_directory):
 
-output_directory = "Outputs/{0}/{1}"
+    playlist = pytube.Playlist(url)
 
-playlist = pytube.Playlist(playlist_url)
+    playlist._video_regex = re.compile(r"\"url\":\"(/watch\?v=[\w-]*)")
 
-playlist._video_regex = re.compile(r"\"url\":\"(/watch\?v=[\w-]*)")
+    for video_url in playlist:
+        video = pytube.YouTube(video_url)
+        print("Downloading {0}".format(video.title))
+        streams = video.streams.filter(file_extension="mp4",progressive=True)
+        max_res = max([stream.resolution for stream in streams])
+        stream = streams.filter(resolution=max_res)[0]
+        stream.download(output_directory)
+        print("Download Complete")
 
-for video_url in playlist:
-    video = pytube.YouTube(video_url)
-    print("Downloading {0}".format(video.title))
-    streams = video.streams.filter(file_extension="mp4",progressive=True)
-    max_res = max([stream.resolution for stream in streams])
-    stream = streams.filter(resolution=max_res)[0]
-    stream.download(output_directory.format(dir_name,start.strftime('%Y-%M-%d')))
-    print("Download Complete")
+def arguments():
+
+    args = argparse.ArgumentParser()
+    args.add_argument("--url")
+    args.add_argument("--output_directory")
+
+    return args.parse_args()
+
+def main():
+    args = arguments()
+    playlist_scraper(
+        url=args.url
+        ,output_directory = args.output_directory
+    )
+
+if __name__ == '__main__':
+    main()
